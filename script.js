c69e79a0ecfabbbc2f981464becfe966cdf416e2
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-13 22:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// complete the given function
+function palindrome(str) {
+  // Normalize: lowercase and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-function palindrome(str){
+  let i = 0;
+  let j = str.length; // Bug fix: was referencing undefined `s` instead of `str`
 
+  while (i < j) {
+    if (str[i] === str[j - 1]) {
+      i++;
+      j--;
+    } else {
+      return false; // Bug fix: simplified, no need for else-if
+    }
+  }
+  return true;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
